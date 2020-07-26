1a68bc48cc7f2470068c53fd1c24c3c361aaf786
Stackbit <projects@stackbit.com>
2020-07-26 10:27:55 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+curl -s -X POST https://api.stackbit.com/project/5f1d59815b3a140017bd585c/webhook/build/pull > /dev/null
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5f1d59815b3a140017bd585c
+fi
+curl -s -X POST https://api.stackbit.com/project/5f1d59815b3a140017bd585c/webhook/build/ssgbuild > /dev/null
+gatsby build
+curl -s -X POST https://api.stackbit.com/project/5f1d59815b3a140017bd585c/webhook/build/publish > /dev/null
